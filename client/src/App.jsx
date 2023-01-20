c876f32d3bc0e6c0a83e02b121a2c9634510ecb1
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-01-19 23:40:15 -0800

    chore: testing code owners

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 const App = () => {
-  return <div className="App">Learn React</div>;
+  return <div className="App">Learn Reacts</div>;
 };
 
 export default App;
