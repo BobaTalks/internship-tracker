88153fb63ababe3f1edd33ce4a91fae46b4e97f5
Joshua Wu <joshua.wu.0910@gmail.com>
2023-01-19 23:31:50 -0800

    chore: add tests ci action (#13)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 const App = () => {
-  return <div className="App" />;
+  return <div className="App">Learn React</div>;
 };
 
 export default App;
