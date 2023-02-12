17c7675898f29d9cc2f31b09256ffd104f0113e8
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-11 17:21:11 -0800

    feat(theme): add theme colors to page and navbar

--- client/src/pages/BasePage.jsx
+++ client/src/pages/BasePage.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Container } from "@mui/material";
+import { Box, Container, useTheme } from "@mui/material";
 import NavBar from "../components/Navbar";
 
 const BasePage = (props) => {
+  const theme = useTheme();
+
   return (
-    <Container>
-      <NavBar></NavBar>
-      {props.children}
-    </Container>
+    <Box sx={{ bgcolor: theme.palette.background.default, height: "100vh" }}>
+      <Container>
+        <NavBar />
+        {props.children}
+      </Container>
+    </Box>
   );
 };
 
