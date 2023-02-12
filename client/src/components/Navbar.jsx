04ff34e9222f1cc9f5cdab10fd39ed7aae07145c
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-02-11 17:31:14 -0800

    chore: fix image preload, use link instead of button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-import { AppBar, Box, Button, Stack, useTheme } from "@mui/material";
-import bobaTalksLogo from "../assets/bobatalks_wide.svg";
-import { Link as RouterLink } from "react-router-dom";
+import React, { useEffect } from "react";
+import { AppBar, Box, Link, Stack, useTheme } from "@mui/material";
+import bobaTalksLogoPath from "../assets/bobatalks_wide.svg";
 
 const NavBar = () => {
   const theme = useTheme();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = bobaTalksLogoPath;
+  }, []);
 
   return (
     <AppBar sx={{ p: 2, bgcolor: theme.palette.background.paper }}>
       <Stack direction="row" sx={{ justifyContent: "space-between" }}>
         <Box>
-          <Button
-            LinkComponent={RouterLink}
+          <Link
             rel="noopener noreferrer"
             target="_blank"
-            to="https://bobatalks.com"
-            variant="text"
+            href="https://bobatalks.com"
+            title="BobaTalk's Website"
           >
-            <img src={bobaTalksLogo} alt="BobaTalks Logo" height="28" />
-          </Button>
+            <img src={bobaTalksLogoPath} alt="BobaTalk's Logo" height="28" />
+          </Link>
           <Box>{/* TODO: add nav links */}</Box>
         </Box>
         <Box>{/* TODO: Right Side Nav Items */}</Box>
