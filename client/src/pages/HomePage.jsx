19d2ebb3b1515cefc0132c5a6aeb8460598c72dd
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-12 19:57:57 -0700

    chore: remove propTypes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import PropTypes from "prop-types";
 import BasePage from "./BasePage";
 
 const HomePage = (props) => {
   return <BasePage>{props.children}</BasePage>;
 };
 
-HomePage.propTypes = {
-  children: PropTypes.node,
-};
 
 export default HomePage;
