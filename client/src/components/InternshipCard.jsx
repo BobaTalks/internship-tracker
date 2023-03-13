74533ee37b55cf826de5ef3dca88649f8c04f774
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-12 20:26:36 -0700

    chore: add filter components, search results, internship cards

---
+++ client/src/components/InternshipCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * https://github.com/BobaTalks/internship-tracker/issues/25
+ * Component for the internship card. should take an Internship as props
+ *
+ */
+const InternshipCard = () => {
+  return <></>;
+};
+
+export default InternshipCard;
