35ec6b6b0e3efb7c8f4fb74c2f4761e5820b04df
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-12 20:13:24 -0700

    chore: add serach results page, add search bar component, add comments for nav bar and home page

---
+++ client/src/pages/SearchResultsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import BasePage from "./BasePage";
+
+/**
+ * https://github.com/BobaTalks/internship-tracker/issues/28
+ * This page will display all the search results. Part of this work we should include the
+ * search and location inputs.
+ */
+const SearchResultsPage = () => {
+  return <BasePage></BasePage>;
+};
+
+export default SearchResultsPage;
