bc1e518149567d9b3ee676703fdf780357c61c60
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-12 21:22:54 -0700

    chore: correct comment

--- client/src/pages/BasePage.jsx
+++ client/src/pages/BasePage.jsx
@@ -7,7 +7,7 @@ import NavBar from "../components/Navbar";
  * Base "Page" component that will wrap and return our standard page layout with
  * navigation at the top.
  *
- * Modifying this component should update all other page components inheriting this.
+ * Modifying this component should update all other page components importing this.
  */
 const BasePage = (props) => {
   const theme = useTheme();
