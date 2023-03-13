f7fefaf7dae12674c2a16050ffe384a3d699c239
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-12 19:58:37 -0700

    chore: add homepagecontent component

--- client/src/pages/BasePage.jsx
+++ client/src/pages/BasePage.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { Box, Container, useTheme } from "@mui/material";
 import NavBar from "../components/Navbar";
 
+/**
+ * Base "Page" component that will wrap and return our standard page layout with
+ * navigation at the top.
+ *
+ * Modifying this component should update all other page components inhereting this.
+ *
+ * @param {*} props
+ * @return {React.Node}
+ */
 const BasePage = (props) => {
   const theme = useTheme();
 
   return (
-    <Box sx={{ bgcolor: theme.palette.background.default, height: "100vh" }}>
+    <Box sx={{ bgColor: theme.palette.background.default, height: "100vh" }}>
       <Container>
         <NavBar />
         {props.children}
