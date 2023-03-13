62be9a4577c950da4b42f165509d765e008cac65
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-03-12 20:15:21 -0700

    chore: add constants file, fix typo in comments

--- client/src/pages/BasePage.jsx
+++ client/src/pages/BasePage.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { Box, Container, useTheme } from "@mui/material";
+import React from "react";
+
 import NavBar from "../components/Navbar";
 
 /**
  * Base "Page" component that will wrap and return our standard page layout with
  * navigation at the top.
  *
- * Modifying this component should update all other page components inhereting this.
+ * Modifying this component should update all other page components inheriting this.
  *
  * @param {*} props
  * @return {React.Node}
