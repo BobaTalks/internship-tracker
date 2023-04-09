12c3193167cf09784fea40ab083e8bcf3986d7ee
leafie8 <ntian518@gmail.com>
2023-04-09 19:38:00 -0400

    completed PostedOn

--- client/src/components/PostedOn.jsx
+++ client/src/components/PostedOn.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Typography } from "@mui/material";
 import { format } from "date-fns";
+import { useTheme } from "@emotion/react";
 
 //assuming the format of the props.date is YYYY-MM-DD
 const PostedOn = (props) => {
   const postedDate = format(new Date(props.date), "M/d/yy");
+  let theme = useTheme();
   return (
-    <Typography sx={{ fontFamily: "Poppins" }}>
+    <p
+      style={{
+        color: theme.palette.text.primary.main,
+        fontSize: theme.fontSize,
+      }}
+    >
       Posted on {postedDate}
-    </Typography>
+    </p>
   );
 };
 
