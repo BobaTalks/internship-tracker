dfee3a4fa8c9544130eb31ce8dbf221ef9fa023b
Jenna Xiao <96795538+JennaXiao3@users.noreply.github.com>
2023-04-15 20:00:19 -0400

    Ticket #23: Add Search Bar (#82)
    * remove react prop-type rule
    
    * initial commit
    
    * addressed pr comments - restructured theme and made minor changes in SearchBar and IconTextField
    
    * added .DS_Store to .gitignore and made grid changes to searchbar and homepage
    
    * removed .DS_Store from .gitignore and made change to search button
    
    ---------
    
    Co-authored-by: Jenna Xiao <jenna_xiao@hotmail.com>

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,55 +1,25 @@
-import { Grid, Typography, useTheme } from "@mui/material";
 import React from "react";
+import { Grid, Stack } from "@mui/material";
 
 import BasePage from "./BasePage";
+import HomePageContent from "../components/HomePageContent";
+import SearchBar from "../components/SearchBar";
 
 /**
  * https://github.com/BobaTalks/internship-tracker/issues/24
  * Will need to include the background styles for the HomePage per design spec
  */
 const HomePage = (props) => {
-  const theme = useTheme();
   return (
     <BasePage>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="flex-start"
-        sx={{ height: "100vh" }}
-        spacing={0}
-      >
-        <Grid item>
-          <Typography
-            variant="title"
-            sx={{
-              [theme.breakpoints.down("sm")]: {
-                fontSize: "3rem",
-                lineHeight: "3rem",
-              },
-            }}
-          >
-            Personalized internship tracking made easy.
-          </Typography>
-        </Grid>
-        <Grid item container direction="row" marginTop={2}>
-          <Grid item sm={7} xs={12}>
-            <Typography
-              variant="body1"
-              sx={{
-                [theme.breakpoints.down("sm")]: {
-                  fontSize: "1rem",
-                },
-              }}
-            >
-              Track when internships open for the company of your choice. Find
-              internships for all the roles you want. Manually track/update your
-              application statuses.
-            </Typography>
+      <Stack spacing={5}>
+        <HomePageContent />
+        <Grid container justifyContent="center">
+          <Grid item lg={8} md={9} xs={11}>
+            <SearchBar />
           </Grid>
-          <Grid item sm={5}></Grid>
         </Grid>
-      </Grid>
+      </Stack>
     </BasePage>
   );
 };
