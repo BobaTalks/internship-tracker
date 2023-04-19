dc5735a1e0ba8cabf20df57adae8dff23698a70d
leafie8 <ntian518@gmail.com>
2023-04-19 18:36:18 -0400

    moving things to search results page

--- client/src/pages/SearchResultsPage.jsx
+++ client/src/pages/SearchResultsPage.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
 
 import BasePage from "./BasePage";
+import PostedOn from "../components/PostedOn";
 
 /**
  * https://github.com/BobaTalks/internship-tracker/issues/28
  * This page will display all the search results. Part of this work we should include the
  * search and location inputs.
  */
+
+const mockData = "02/14/2003";
+
 const SearchResultsPage = () => {
-  return <BasePage></BasePage>;
+  return (
+    <BasePage>
+      <Stack spacing={2}>
+        <Typography variant="pageTitle" marginTop="6rem">
+          Find Internships
+        </Typography>
+        <PostedOn date={mockData} />
+      </Stack>
+    </BasePage>
+  );
 };
 
 export default SearchResultsPage;
