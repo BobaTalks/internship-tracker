bc64eba9454d6165f2df59597ab620367af27f57
leafie8 <ntian518@gmail.com>
2023-04-30 17:13:38 -0400

    completion of postedOn

--- client/src/components/PostedOn.jsx
+++ client/src/components/PostedOn.jsx
@@ -5,29 +5,7 @@ import { useTheme } from "@mui/material";
 //assuming the format of the props.date is YYYY-MM-DD
 const PostedOn = (props) => {
   const theme = useTheme();
-  return (
-    <Typography
-      variant="body1"
-      sx={{
-        [theme.breakpoints.down("md")]: {
-          fontSize: "0.5rem",
-          lineHeight: "1.5rem",
-          marginTop: "8rem",
-        },
-      }}
-    >
-      Posted on {props.date}
-    </Typography>
-    /*
-    <p
-      style={{
-        color: theme.palette.text.primary.main,
-        fontSize: theme.fontSize,
-      }}
-    >
-      Posted on {props.date}
-    </p>*/
-  );
+  return <Typography variant="body1"> Posted on {props.date}</Typography>;
 };
 
 export default PostedOn;
