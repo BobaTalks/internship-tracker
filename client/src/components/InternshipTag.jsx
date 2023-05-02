222b56f10447bc287d1daad6021c16104abc46b3
Jessica Chen <j2286che@uwaterloo.ca>
2023-05-02 16:00:33 -0400

    Updated CSS and cleaned up code

--- client/src/components/InternshipTag.jsx
+++ client/src/components/InternshipTag.jsx
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 import { Box, useTheme } from "@mui/material";
 
 const InternshipTag = (props) => {
-  let theme = useTheme();
+  const theme = useTheme();
+  const Icon = props.icon;
 
   return (
-    <>
-      <Box
-        display="flex"
-        maxWidth={false}
-        sx={{
-          borderRadius: "0.5rem",
-          padding: "0.5rem 1rem",
-          bgcolor: theme.palette.blue.lighter,
-          color: theme.palette.text.primary,
-          border: 1,
-          maxWidth: "fit-content",
-        }}
-      >
-        <props.icon />
-        <Box sx={{ padding: "0rem 0.25rem" }}>{props.label}</Box>
-      </Box>
-    </>
+    <Box
+      display="flex"
+      maxWidth={false}
+      sx={{
+        borderRadius: "0.5rem",
+        padding: "0.5rem 1rem",
+        bgcolor: "blue.50",
+        color: "gray.800",
+        border: 1,
+        borderColor: "gray.800",
+        maxWidth: "fit-content",
+      }}
+    >
+      <Icon />
+      <Box sx={{ padding: "0rem 0.25rem" }}>{props.label}</Box>
+    </Box>
   );
 };
 
