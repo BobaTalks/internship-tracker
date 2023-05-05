78f5cb20f16c93af68b75e8ab5046d40ac007516
leafie8 <ntian518@gmail.com>
2023-05-04 20:05:20 -0400

    removed unused theme constant

--- client/src/components/PostedOn.jsx
+++ client/src/components/PostedOn.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Typography } from "@mui/material";
-import { useTheme } from "@mui/material";
 
 //assuming the format of the props.date is YYYY-MM-DD
 const PostedOn = (props) => {
-  const theme = useTheme();
-  return <Typography variant="body1"> Posted on {props.date}</Typography>;
+  return <Typography variant="body1">Posted on {props.date}</Typography>;
 };
 
 export default PostedOn;
