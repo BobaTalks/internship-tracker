55ff7e144062b04a63dde693f21ff52fd72cb9f1
leafie8 <ntian518@gmail.com>
2023-05-07 20:10:29 -0400

    working on adding ReadMore button and caret up

--- client/src/components/JobDescription.jsx
+++ client/src/components/JobDescription.jsx
@@ -1,5 +1,46 @@
-import React from "react";
-import { Grid, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Grid, Typography, Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Expand } from "@mui/icons-material";
+
+const ExpandLess = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(180deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+const ReadMore = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    if (isReadMore) setIsReadMore(!isReadMore);
+  };
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    if (!isReadMore) {
+      setExpanded(!expanded);
+      setIsReadMore(!isReadMore);
+    }
+  };
+  return (
+    <p className="text">
+      <Typography>{isReadMore ? text.slice(0, 150) : text}</Typography>
+      <Button onClick={toggleReadMore} className="read-or-hide">
+        {"Read more"}
+      </Button>
+      <ExpandLess expand={expanded} onClick={handleExpandClick}>
+        <ExpandMoreIcon />
+      </ExpandLess>
+    </p>
+  );
+};
 
 const JobDescription = (props) => {
   return (
@@ -7,8 +48,10 @@ const JobDescription = (props) => {
       <Grid item>
         <Typography variant="h5">Job description</Typography>
       </Grid>
-      <Grid item container direction="row" marginTop={2}>
-        <Typography variant="body1"> {props.description}</Typography>
+      <Grid item>
+        <Typography variant="body1">
+          <ReadMore>{props.description}</ReadMore>
+        </Typography>
       </Grid>
     </Grid>
   );
