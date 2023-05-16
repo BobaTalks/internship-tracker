f8eddec21519d00ebd454a4a1a563ce359be90ff
Anthony Camarillo <58638902+a-camarillo@users.noreply.github.com>
2023-05-16 16:13:06 -0700

    (feat): add functionality for keyword search (#127)

--- server/webserver/queries.py
+++ server/webserver/queries.py
@@ -1,5 +1,6 @@
 from webserver.extensions import db
 from webserver.models import User, Job, Location
+from utils.jobs_utils import create_job_object
 
 
 def create_user(email, name, hash, authentication_method):
@@ -36,8 +37,61 @@ def create_job(title, url, city, state, country):
     db.session.commit()
 
 
+def get_jobs(params):
+    if not params["title"] and not params["location"]:
+        job_object_arr = []
+        query_results = db.session.execute(
+            db.select(Job, Location).join(Location, Job.location == Location.id)
+        )
+        for result in query_results:
+            result_obj = create_job_object(result)
+            job_object_arr.append(result_obj)
+        return job_object_arr
+    if not params["title"] and params["location"]:
+        job_object_arr = []
+        query_results = db.session.execute(
+            db.select(Job, Location)
+            .join(Location, Job.location == Location.id)
+            .where(
+                (Location.city.icontains(params["location"]))
+                | (Location.state.icontains(params["location"]))
+                | (Location.country.icontains(params["location"]))
+            )
+        )
+        for result in query_results:
+            result_obj = create_job_object(result)
+            job_object_arr.append(result_obj)
+        return job_object_arr
+    if not params["location"] and params["title"]:
+        job_object_arr = []
+        query_results = db.session.execute(
+            db.select(Job, Location)
+            .join(Location, Job.location == Location.id)
+            .where((Job.title.icontains(params["title"])))
+        )
+        for result in query_results:
+            result_obj = create_job_object(result)
+            job_object_arr.append(result_obj)
+        return job_object_arr
+    else:
+        job_object_arr = []
+        query_results = db.session.execute(
+            db.select(Job, Location)
+            .join(Location, Job.location == Location.id)
+            .where((Job.title.icontains(params["title"])))
+            .where(
+                (Location.city.icontains(params["location"]))
+                | (Location.state.icontains(params["location"]))
+                | (Location.country.icontains(params["location"]))
+            )
+        )
+        for result in query_results:
+            result_obj = create_job_object(result)
+            job_object_arr.append(result_obj)
+        return job_object_arr
+
+
 def get_job(id):
-    jobject = {}
     query_result = (
         db.session.execute(
             db.select(Job, Location)
@@ -47,15 +101,8 @@ def get_job(id):
         .first()
         ._asdict()
     )
-    jobject["id"] = query_result["Job"].id
-    jobject["title"] = query_result["Job"].title
-    jobject["url"] = query_result["Job"].url
-    jobject["location"] = {
-        "city": query_result["Location"].city,
-        "state": query_result["Location"].state,
-        "country": query_result["Location"].country,
-    }
-    return jobject
+    job_object = create_job_object(query_result)
+    return job_object
 
 
 def delete_job(id):
