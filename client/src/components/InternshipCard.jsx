273d23276fc6ec427e54fb53a951bff406772e80
Jenna Xiao <jenna_xiao@hotmail.com>
2023-10-01 15:47:08 -0400

    added all components to internship post card, as well as some mock data

--- client/src/components/InternshipCard.jsx
+++ client/src/components/InternshipCard.jsx
@@ -1,16 +1,87 @@
-import { Card, CardContent } from '@mui/material';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { Card, CardContent, Grid } from '@mui/material';
 import React from 'react';
 
+import InternshipCompanyInfo from './InternshipCompanyInfo';
+import InternshipTag from './InternshipTag';
+import JobDescription from './JobDescription';
+import PostedOn from './PostedOn';
 /**
  * https://github.com/BobaTalks/internship-tracker/issues/25
  * Component for the internship card. should take an Internship as props
  *
  */
 
-const InternshipCard = () => {
+const InternshipCard = ({
+  companyName,
+  position,
+  location,
+  labels,
+  datePosted,
+  jobDesc,
+  jobReqs,
+  jobResp,
+  jobLink,
+}) => {
+  const getLabelIcon = (filter) => {
+    // returns the correct icon depending on the corresponding filter the tag is showing
+    return <AccessTimeIcon />;
+  };
   return (
     <Card>
-      <CardContent />
+      <CardContent>
+        <Grid container direction="column">
+          <Grid
+            direction="row"
+            container
+            item
+            spacing={3}
+            justifyContent="space-between"
+          >
+            <Grid item xs={7}>
+              <InternshipCompanyInfo
+                name={companyName}
+                title={position}
+                location={location}
+              />
+            </Grid>
+            <Grid
+              container
+              spacing={2}
+              item
+              direction="row"
+              xs={5}
+              alignItems="top"
+              justifyContent="right"
+            >
+              {labels.map((label, i) => {
+                return (
+                  <InternshipTag
+                    label={label.name}
+                    icon={getLabelIcon(label.filter)}
+                    key={i}
+                  />
+                );
+              })}
+            </Grid>
+          </Grid>
+          <Grid container item direction="row" mt={8}>
+            <Grid item>
+              <PostedOn date={datePosted} />
+            </Grid>
+          </Grid>
+          <Grid container item direction="row" mt={8}>
+            <Grid item>
+              <JobDescription
+                description={jobDesc}
+                requirements={jobReqs}
+                responsibilities={jobResp}
+                externalLink={jobLink}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
--- client/src/components/InternshipTag.jsx
+++ client/src/components/InternshipTag.jsx
@@ -1,25 +1,29 @@
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React from 'react';
 
 const InternshipTag = (props) => {
-  const Icon = props.icon;
+  const icon = props.icon;
 
   return (
-    <Box
-      display="flex"
-      maxWidth={false}
-      sx={{
-        borderRadius: '0.5rem',
-        padding: '0.5rem 1rem',
-        bgcolor: 'secondary.light',
-        border: 1,
-        borderColor: 'text.main',
-        maxWidth: 'fit-content',
-      }}
-    >
-      <Icon />
-      <Box sx={{ padding: '0rem 0.25rem' }}>{props.label}</Box>
-    </Box>
+    <Grid item>
+      <Box
+        display="flex"
+        maxWidth={false}
+        sx={{
+          borderRadius: '0.5rem',
+          padding: '0.5rem 1rem',
+          bgcolor: 'secondary.light',
+          border: 1,
+          borderColor: 'text.main',
+          maxWidth: 'fit-content',
+          height: 'fit-content',
+          textTransform: 'uppercase',
+        }}
+      >
+        {icon}
+        <Box sx={{ padding: '0rem 0.25rem' }}>{props.label}</Box>
+      </Box>
+    </Grid>
   );
 };
 
