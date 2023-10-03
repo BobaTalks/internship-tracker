ddf39d52d4ae5824cddfad824ee17bf1e0d4fb42
Jenna Xiao <96795538+JennaXiao3@users.noreply.github.com>
2023-10-03 19:02:16 -0400

    feat: Create Internship Post Card (#157)
    * added all components to internship post card, as well as some mock data
    
    * resolved comments with fixes

--- client/src/components/InternshipTag.jsx
+++ client/src/components/InternshipTag.jsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 const InternshipTag = (props) => {
-  const Icon = props.icon;
+  const icon = props.icon;
 
   return (
     <Box
@@ -15,9 +15,11 @@ const InternshipTag = (props) => {
         border: 1,
         borderColor: 'text.main',
         maxWidth: 'fit-content',
+        height: 'fit-content',
+        textTransform: 'uppercase',
       }}
     >
-      <Icon />
+      {icon}
       <Box sx={{ padding: '0rem 0.25rem' }}>{props.label}</Box>
     </Box>
   );
--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,3 +1,4 @@
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
 import InternshipCard from './InternshipCard';
@@ -7,8 +8,30 @@ import InternshipCard from './InternshipCard';
  * Import and render InternshipCard for all internships in Internship[]. Will need mock
  * data or API available
  */
-const SearchResults = () => {
-  return <InternshipCard />;
+const SearchResults = ({ internships }) => {
+  return (
+    <Stack direction="column" spacing={5} paddingTop={5}>
+      <Typography variant="errorMessage">
+        {internships.length} results
+      </Typography>
+      {internships.map((info, i) => {
+        return (
+          <InternshipCard
+            companyName={info.companyName}
+            position={info.position}
+            location={info.location}
+            labels={info.labels}
+            datePosted={info.datePosted}
+            jobDesc={info.jobInfo.jobDesc}
+            jobReqs={info.jobInfo.jobReqs}
+            jobResp={info.jobInfo.jobResp}
+            jobLink={info.jobInfo.jobLink}
+            key={i}
+          />
+        );
+      })}
+    </Stack>
+  );
 };
 
 export default SearchResults;
