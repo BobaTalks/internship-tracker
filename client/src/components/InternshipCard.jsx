75982e7378d43a328197561c03f1b3440f7f7775
Jenna Xiao <jenna_xiao@hotmail.com>
2023-11-01 20:03:53 -0400

    adjust margin between components in internship card

--- client/src/components/InternshipCard.jsx
+++ client/src/components/InternshipCard.jsx
@@ -66,7 +66,7 @@ const InternshipCard = ({
           </Grid>
           <Grid
             item
-            mt={{ xs: 4, sm: 8 }}
+            mt={4}
             direction="row"
             container
             justifyContent="space-between"
@@ -76,7 +76,7 @@ const InternshipCard = ({
             <PostedOn date={datePosted} />
             <SaveAndApply link={jobLink} isSaved={isSaved} />
           </Grid>
-          <Grid item mt={4}>
+          <Grid item mt={{ xs: 4, sm: 2 }}>
             <JobDescription
               description={jobDesc}
               requirements={jobReqs}
