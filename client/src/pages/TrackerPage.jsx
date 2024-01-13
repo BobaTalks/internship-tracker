6770f4a2957626018887ee15bff1515ef492c875
Jenna Xiao <jenna_xiao@hotmail.com>
2024-01-13 15:51:25 -0500

    added drag and drop feature for tracker page

--- client/src/pages/TrackerPage.jsx
+++ client/src/pages/TrackerPage.jsx
@@ -1,5 +1,6 @@
 import { Box, Divider, Grid, Typography } from '@mui/material';
 import React, { useState } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 
 import IconTextField from '../components/IconTextField';
 import SearchButton from '../components/SearchButton';
@@ -7,9 +8,53 @@ import TrackerColumn from '../components/TrackerColumn';
 import BasePage from './BasePage';
 
 const TrackerPage = () => {
+  const [trackerItems, setTrackerItems] = useState({
+    saved: ['saved1', 'saved2'],
+    applied: ['applied1', 'applied2', 'applied3'],
+    responded: ['responded1', 'responded2', 'responded3', 'responded4'],
+    archived: ['archived1', 'archived2', 'archived3'],
+  });
   const [search, setSearch] = useState('');
   const handleClick = () => {};
   const CHANGE_PLACEHOLDER_WIDTH = 630;
+
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    if (
+      !destination ||
+      (destination.droppableId === source.droppableId &&
+        destination.index === source.index)
+    ) {
+      return;
+    }
+
+    const sInd = source.droppableId;
+    const dInd = destination.droppableId;
+
+    if (sInd === dInd) {
+      const newOrder = Array.from(trackerItems[sInd]);
+      newOrder.splice(source.index, 1);
+      newOrder.splice(destination.index, 0, JSON.parse(draggableId));
+
+      setTrackerItems((prevState) => ({
+        ...prevState,
+        [sInd]: newOrder,
+      }));
+    } else {
+      const sourceCol = Array.from(trackerItems[sInd]);
+      const destCol = Array.from(trackerItems[dInd]);
+
+      const [removed] = sourceCol.splice(source.index, 1);
+      destCol.splice(destination.index, 0, removed);
+
+      setTrackerItems((prevState) => ({
+        ...prevState,
+        [sInd]: sourceCol,
+        [dInd]: destCol,
+      }));
+    }
+  };
   return (
     <BasePage>
       <Box
@@ -42,13 +87,18 @@ const TrackerPage = () => {
           mt={6}
           sx={{ flexGrow: 1 }}
         >
-          <TrackerColumn category="Saved" />
-          <Divider orientation="vertical" />
-          <TrackerColumn category="Applied" />
-          <Divider orientation="vertical" />
-          <TrackerColumn category="Responded" />
-          <Divider orientation="vertical" />
-          <TrackerColumn category="Archived" />
+          <DragDropContext onDragEnd={onDragEnd}>
+            <TrackerColumn category="Saved" cards={trackerItems.saved} />
+            <Divider orientation="vertical" />
+            <TrackerColumn category="Applied" cards={trackerItems.applied} />
+            <Divider orientation="vertical" />
+            <TrackerColumn
+              category="Responded"
+              cards={trackerItems.responded}
+            />
+            <Divider orientation="vertical" />
+            <TrackerColumn category="Archived" cards={trackerItems.archived} />
+          </DragDropContext>
         </Box>
       </Box>
     </BasePage>
