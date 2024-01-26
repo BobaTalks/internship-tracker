0f2b63b7eeba0c45adbd4af1cfc9b084f0af880c
Jenna Xiao <96795538+JennaXiao3@users.noreply.github.com>
2024-01-26 05:09:00 -0500

    feat: Internship card for internship tracker (#187)
    * creater internship cards for tracker page
    
    * small touchups

--- client/src/pages/TestPage.jsx
+++ client/src/pages/TestPage.jsx
@@ -1,55 +1,28 @@
-import { Button } from '@mui/material';
-import { signInWithPopup } from 'firebase/auth';
+import { Box, Stack } from '@mui/material';
 import React, { useContext } from 'react';
 
-// This is a test page - will be replaced by sign in / sign out
-import SignOutButton from '../components/SignOutButton';
-import AuthContext from '../contexts/AuthContext';
-import { auth, provider } from '../utils/firebaseConfig';
+import TrackerCard from '../components/TrackerCard';
+import TrackerContext from '../contexts/TrackerContext';
 import BasePage from './BasePage';
 
+// This is a test page - you can test any components you wish here
+
 const TestPage = () => {
-  const [userIn, setUserIn] = useContext(AuthContext);
-
-  const signInUser = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
-
-        // The signed-in user info.
-        // const user = result.user;
-        // IdP data available using getAdditionalUserInfo(result)
-        // ...
-        setUserIn(true);
-      })
-      .catch((error) => {
-        // Handle Errors here.
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // The email of the user's account used.
-        // const email = error.customData.email;
-        // The AuthCredential type that was used.
-        // const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
-  };
-
+  const [trackedInternships] = useContext(TrackerContext);
   return (
     <BasePage>
-      {userIn ? (
-        <SignOutButton />
-      ) : (
-        <Button
-          variant="rounded"
-          color="thai"
-          onClick={signInUser}
-          sx={{ mt: '10rem', height: '3rem', width: '8rem' }}
-        >
-          Sign In
-        </Button>
-      )}
+      <Box mt="10rem">
+        <Stack gap={3}>
+          {trackedInternships.map((internship) => (
+            <TrackerCard
+              key={internship.id}
+              id={internship.internshipId}
+              dateAdded={internship.dateAdded}
+              appliedDate={internship.appliedDate}
+            />
+          ))}
+        </Stack>
+      </Box>
     </BasePage>
   );
 };
